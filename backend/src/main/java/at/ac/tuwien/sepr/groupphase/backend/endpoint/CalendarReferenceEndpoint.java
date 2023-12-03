2e30178de0657e691390e9aebffe2763a7fdc454
Julian Schwendinger <e12123444@student.tuwien.ac.at>
2023-12-03 11:22:34 +0100

    feat(backend): added export @GetMapping
    it returns a dummy ics file as the actual parsing is nyi
    
    Refs: #12

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/CalendarReferenceEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/CalendarReferenceEndpoint.java
@@ -9,6 +9,11 @@ import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,7 +23,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.lang.invoke.MethodHandles;
-import java.net.MalformedURLException;
 
 @RestController
 @RequestMapping(value = "/api/v1/calendar")
@@ -48,7 +52,50 @@ public class CalendarReferenceEndpoint {
     @GetMapping("/{id}")
     @Operation(summary = "Get a stored CalendarReference", security = @SecurityRequirement(name = "apiKey"))
     public CalendarReferenceDto getCalendarReference(@PathVariable Long id) {
-        LOGGER.info("Get /api/v1/calendar/get/{}", id);
+        LOGGER.info("Get /api/v1/calendar/{}", id);
         return calendarReferenceMapper.calendarReferenceToDto(calendarReferenceService.getFromId(id));
     }
+
+    /**
+     * <p> Exports the Calendar associated with the given token.</p>
+     * <p> Tokens are specific to a user or a tagged subset of their managed calendar. </p>
+
+     * @param token the user/tag specific token
+     * @return ics file containing the adjusted calendar
+     */
+    @GetMapping("/export/{token}")
+    @Operation(summary = "Export a calender from its url")
+    public ResponseEntity<Resource> exportCalendarFile(@PathVariable String token) {
+        LOGGER.info("Get /api/v1/calendar/get/export/{location}");
+
+        String icsContent = """
+            BEGIN:VCALENDAR
+            VERSION:2.0
+            PRODID:-//Your Organization//Your Application//EN
+            CALSCALE:GREGORIAN
+            BEGIN:VEVENT
+            SUMMARY:Dummy Event
+            DESCRIPTION:This is a dummy event for a dummy calendar.
+            CATEGORIES:HOLIDAY
+            DTSTART:20000101T000000Z
+            DTEND:20500101T000000Z
+            UID:unique-event-id-123456789
+            SEQUENCE:0
+            STATUS:CONFIRMED
+            TRANSP:OPAQUE
+            END:VEVENT
+            END:VCALENDAR
+            """;
+
+        byte[] fileContent = icsContent.getBytes();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + token + ".ics");
+
+        return ResponseEntity.ok()
+                             .headers(headers)
+                             .contentLength(fileContent.length)
+                             .contentType(MediaType.parseMediaType("text/calendar"))
+                             .body(new ByteArrayResource(fileContent));
+    }
+
 }
