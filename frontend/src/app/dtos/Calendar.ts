d15266c0922bee64589cd482b07f6876d2aae844
Maximilian Selle <e12021348@student.tuwien.ac.at>
2023-12-05 17:12:15 +0100

    feat(frontend): change ui + add custom events

--- frontend/src/app/dtos/Calendar.ts
+++ frontend/src/app/dtos/Calendar.ts
@@ -3,6 +3,14 @@ export class Calendar{
     public name: string,
     public id: number,
     public color: string,
-    public isActive: boolean
+    public isActive: boolean,
+    public events?: MyCalendarEvent[]
   ) {}
+}
+
+export interface MyCalendarEvent{
+   title: string,
+   id?: number,
+   start: Date,
+   end?: Date
 }
---
+++ frontend/src/app/dtos/Configuration.ts
@@ -0,0 +1,8 @@
+export class Configuration{
+    constructor(
+      public name: string,
+      public id: number,
+      public color: string,
+      public isActive: boolean
+    ) {}
+  }
