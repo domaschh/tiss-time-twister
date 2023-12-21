f7116b0ff710709bf82bf3bbd7e838c85bc40086
Domasch <magesthomas@gmail.com>
2023-12-21 09:38:48 +0100

    add deleting calendar functionality

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/CalendarReferenceRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/CalendarReferenceRepository.java
@@ -4,6 +4,8 @@ import at.ac.tuwien.sepr.groupphase.backend.entity.CalendarReference;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CalendarReferenceRepository extends JpaRepository<CalendarReference, Long> {
 }
