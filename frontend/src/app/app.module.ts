1e1ae3045eaf217ec792e7f4e83c5e55e6c8bcf1
Domasch <magesthomas@gmail.com>
2023-12-30 22:00:54 +0100

    feat: add tooltips

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -33,6 +33,7 @@ import { ConfigModalComponent } from './components/public-config-card/config-mod
 import { RuleFoldComponent } from './components/public-config-card/rule-fold/rule-fold.component';
 import { NavigationPopoverComponent } from './components/navbar/navigation-popover/navigation-popover.component';
 import { NavigationPopoverRightComponent } from './components/navbar/navigation-popover-right/navigation-popover-right.component';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [
@@ -63,6 +64,7 @@ import { NavigationPopoverRightComponent } from './components/navbar/navigation-
     AppRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
+    MatTooltipModule,
     NgbModule,
     FormsModule,
     CalendarModule.forRoot({ provide: DateAdapter, useFactory: adapterFactory }),
