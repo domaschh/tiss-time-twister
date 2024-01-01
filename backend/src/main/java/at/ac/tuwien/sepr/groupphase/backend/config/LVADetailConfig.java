26f56ec848627df256288b807236e6e9b603ee1a
Domasch <magesthomas@gmail.com>
2024-01-01 15:07:05 +0100

    feat: add new lva name mapping

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/config/LVADetailConfig.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/config/LVADetailConfig.java
@@ -12,20 +12,39 @@ import java.util.Map;
 
 @Configuration
 public class LVADetailConfig {
-    private static final String FILE_PATH = "shorthands.txt"; // replace with your file path
+    private static final String BACHELOR_SHORTHANDS = "bachelor_shorthands.txt"; // replace with your file path
+    private static final String MASTER_SHORTHANDS = "master_softwareengineering_shorthands.txt"; // replace with your file path
     private static final String LVA_PATH = "lvas.txt"; // replace with your file path
 
     @Bean
     public Map<String, LVADetail> shorthandMap() {
         Map<String, LVADetail> map = new HashMap<>();
-        try (BufferedReader reader = new BufferedReader(new FileReader(FILE_PATH))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(BACHELOR_SHORTHANDS))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 String[] parts = line.split(",");
-                if (parts.length >= 3) {
-                    String shorthand = parts[0];
-                    String germanName = parts[1];
-                    String englishName = parts[2];
+                if (parts.length >= 2) {
+                    String shorthand = parts[1];
+                    String germanName = parts[0];
+                    String englishName = parts[0];
+                    var detail = new LVADetail(shorthand, "https://tiss.tuwien.ac.at/", "123.456");
+
+                    map.put(germanName.trim(), detail);
+                    map.put(englishName.trim(), detail);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(MASTER_SHORTHANDS))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                if (parts.length >= 2) {
+                    String shorthand = parts[1];
+                    String germanName = parts[0];
+                    String englishName = parts[0];
                     var detail = new LVADetail(shorthand, "https://tiss.tuwien.ac.at/", "123.456");
 
                     map.put(germanName.trim(), detail);
@@ -41,15 +60,17 @@ public class LVADetailConfig {
             while ((line = reader.readLine()) != null) {
                 String[] parts = line.split(","); // Use comma as the delimiter
                 if (parts.length >= 4) {
+                    String lva_type = parts[1];
                     String lvaNumber = parts[0];  // LVA number is the first part
                     String lvaName = parts[2];  // LVA number is the first part
                     String url = parts[3];        // URL is the fourth part
-                    if (map.containsKey(lvaName) && lvaName.equals("Funktionale Programmierung")) {
-                        LVADetail existingDetail = map.get(lvaName);
+
+                    if (map.containsKey(lva_type + " " + lvaName)) {
+                        LVADetail existingDetail = map.get(lva_type + " " + lvaName);
 
                         LVADetail updatedDetail = new LVADetail(existingDetail.shorthand(), url, lvaNumber);
 
-                        map.replace(lvaName, updatedDetail);
+                        map.replace(lva_type + " " + lvaName, updatedDetail);
                     }
                 }
             }
