0df4dfb3acd44a136b4d7673b0a41619d943273e
Domasch <magesthomas@gmail.com>
2024-01-26 14:51:35 +0100

    trytofix method origins

--- backend/target/generated-sources/annotations/at/ac/tuwien/sepr/groupphase/backend/endpoint/mapper/CalendarReferenceMapperImpl.java
+++ backend/target/generated-sources/annotations/at/ac/tuwien/sepr/groupphase/backend/endpoint/mapper/CalendarReferenceMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-01-26T14:29:06+0100",
+    date = "2024-01-26T14:42:26+0100",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.1 (Oracle Corporation)"
 )
 @Component
