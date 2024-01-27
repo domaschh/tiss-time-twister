8870306b362823d855831cff70e341dc935f19e8
Domasch <magesthomas@gmail.com>
2024-01-27 17:39:07 +0100

    frontend backend ulr

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // backendUrl: 'https://tiss-time-twister-production.up.railway.app/api/v1'
-  backendUrl: 'http://localhost:8080/api/v1'
+  backendUrl: 'https://tiss-time-twister-production.up.railway.app/api/v1'
+  // backendUrl: 'http://localhost:8080/api/v1'
 };
 
 /*
