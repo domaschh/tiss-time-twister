3bc63556dda4d794ec9f268d485af8d02d033b86
Domasch <magesthomas@gmail.com>
2024-01-29 13:16:29 +0100

    fix resetpassword prod

--- backend/target/generated-sources/annotations/at/ac/tuwien/sepr/groupphase/backend/endpoint/mapper/CalendarReferenceMapperImpl.java
+++ backend/target/generated-sources/annotations/at/ac/tuwien/sepr/groupphase/backend/endpoint/mapper/CalendarReferenceMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-01-29T12:23:16+0100",
+    date = "2024-01-29T13:16:24+0100",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.1 (Oracle Corporation)"
 )
 @Component
